4cd20dfd4d9f80ee5f474f6b5ab33686ae0ab163
yesunboy <yebohuangsunboy@163.com>
2017-08-30 15:04:47 +0800

    c3p0

--- src/main/java/com/ybh/Test.java
+++ src/main/java/com/ybh/Test.java
@@ -1,17 +1,40 @@
 package com.ybh;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 @Controller
-public class Test{
-    
-    @RequestMapping("/hello")
-    private String Index(){
-        
-        return "ok";
-    }
-    
+public class Test {
+
+	@RequestMapping("/hello")
+	private String Index(String username, String password, Model model) {
+		System.out.println("初始化context....开始");
+		ApplicationContext context = new ClassPathXmlApplicationContext("config/spring-mvc.xml");
+		ComboPooledDataSource cpds = (ComboPooledDataSource) context.getBean("oracle");
+		try {
+			Connection conn = cpds.getConnection();
+			PreparedStatement ps = conn.prepareStatement("select * from YBH_LY_IMP_GOODS");
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				System.out.println(rs.getString("新货品id"));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("初始化context....结束");
+		model.addAttribute("usernamecc", username);
+		return "ok";
+	}
+
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,13 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>  
+<!DOCTYPE html> 
 <html>
 <body>
-<h2>Hello World!</h2>
+<form action="hello" method="post">  
+    username:<input type="text" name="username">  
+    <br/>  
+    password:<input type="text" name="password">  
+    <br/>  
+    <input type="submit" value="登录">  
+</form>  
 </body>
 </html>
